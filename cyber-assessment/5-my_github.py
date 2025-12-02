a260b8c282ece0de580d96bf02501b4e6f2156f8
Luca Ferreira Costa <101402096+lucafrrc@users.noreply.github.com>
2025-12-02 15:13:27 +0000

    test 6

--- cyber-assessment/5-my_github.py
+++ cyber-assessment/5-my_github.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
+
 import sys
 import requests
 
-response = requests.get("https://api.github.com/user", auth=(sys.argv[1], sys.argv[2]))
 
-if response.status_code == 200:
-    print(response.json().get("id"))
-else:
-    print("None")
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    
+    url = "https://api.github.com/user"
+    
+    response = requests.get(url, auth=(username, password))
+    
+    if response.status_code == 200:
+        user_data = response.json()
+        print(user_data.get('id'))
+    else:
+        print(None)
